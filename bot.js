941fe3f905faee14fb4fa2eaa39d495781054535
Solrian <dennis.pfaff@web.de>
2020-01-17 21:49:01 +0100

    diferrent discord servers for master and develop

--- bot.js
+++ bot.js
@@ -4,15 +4,42 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+
+	console.log("Servers:")
+
+	client.guilds.forEach((guild) =>
+	{
+		console.log(" - " + guild.name)
+		console.log(" - " + guild.id)
+	})
+
     console.log('I am ready!');
 });
 
 client.on('message', message => 
 {
+	if (message.author == client.user)
+	{
+		//bot soll nicht auf eigene Nachrichten reagieren
+		return
+	}
+
+	if (message.guild.id !== process.env.GUILD_ID)
+	{
+		//bot soll nur auf Nachrichten auf einen spezifischen Server reagieren
+		return
+	}
+
+	if (message.channel.id !== process.env.CHANNEL_ID)
+	{
+		//bot soll nur auf Nachrichten in einem spezfischen Channel reagieren
+		return
+	}
+
 	if (message.content === 'ping') 
 	{
-    message.reply('pong');
-  }
+		message.reply('pong');
+	}
 	else if (message.content === 'hallo') 
 	{
 		message.reply('hi');
