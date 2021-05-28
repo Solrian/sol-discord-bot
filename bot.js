662508bc4c4c2ed79fdad0578f751122ee0aea06
Solrain <dennis.pfaff@web.de>
2021-05-28 11:09:34 +0200

    package updates, token in config.json ausgelagert

--- bot.js
+++ bot.js
@@ -1,49 +1,9 @@
-const Dotenv = require('dotenv').config();
-
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const fs = require('fs');
 
-client.on('ready', () => {
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
-	console.log("Servers:")
+var client = new Discord.Client();
 
-	client.guilds.forEach((guild) =>
-	{
-		console.log(" - " + guild.name)
-		console.log(" - " + guild.id)
-	})
 
-    console.log('I am ready!');
-});
-
-client.on('message', message => 
-{
-	if (message.author == client.user)
-	{
-		//bot soll nicht auf eigene Nachrichten reagieren
-		return
-	}
-
-	if (message.guild.id !== process.env.GUILD_ID)
-	{
-		//bot soll nur auf Nachrichten auf einen spezifischen Server reagieren
-		return
-	}
-
-	if (message.channel.id !== process.env.CHANNEL_ID)
-	{
-		//bot soll nur auf Nachrichten in einem spezfischen Channel reagieren
-		return
-	}
-
-	if (message.content === 'ping') 
-	{
-		message.reply('pong');
-	}
-	else if (message.content === 'hallo') 
-	{
-		message.reply('hi');
-	}
-});
-
-client.login(process.env.BOT_TOKEN);
+client.login(config.token);
